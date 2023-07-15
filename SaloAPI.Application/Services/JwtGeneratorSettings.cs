fd4bcd283876e1a0c9f745062d22e62a4c6c3b1e
kravchenkoegorii <kravchenko13138@gmail.com>
2023-07-15 23:01:14 +0300

    added jwt service

---
+++ SaloAPI.Application/Services/JwtGeneratorSettings.cs
@@ -0,0 +1,17 @@
+﻿using SaloAPI.Application.Interfaces;
+
+namespace SaloAPI.Application.Services;
+
+public class JwtGeneratorSettings : IJwtGeneratorSettings
+{
+    public JwtGeneratorSettings(
+        string saloJwtSignKey, 
+        int saloJwtLifetimeMinutes)
+    {
+        SaloJwtSignKey = saloJwtSignKey;
+        SaloJwtLifetimeMinutes = saloJwtLifetimeMinutes;
+    }
+
+    public string SaloJwtSignKey { get; }
+    public int SaloJwtLifetimeMinutes { get; }
+}
