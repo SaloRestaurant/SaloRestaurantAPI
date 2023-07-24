c2c7148037c60526344aa67b66a14494d974232f
Егор Кравченко <kravchenko13138@gmail.com>
2023-07-25 01:31:29 +0300

    added command executor

---
+++ SaloAPI.BusinessLogic/Configuration/SaloCommandExecutor.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using SaloAPI.BusinessLogic.Responses;
+
+namespace SaloAPI.BusinessLogic.Configuration;
+
+public static class SaloCommandExecutor
+{
+    internal static async Task<Result<TResponse>> RequestAsync<TResponse>(
+        IRequest<Result<TResponse>> request,
+        CancellationToken cancellationToken) where TResponse : ResponseBase
+    {
+        var scope = SaloCompositionRoot.CreateScope();
+        var mediator = scope.ServiceProvider.GetRequiredService<IMediator>();
+        var result = await mediator.Send(request, cancellationToken);
+
+        return result;
+    }
+}
